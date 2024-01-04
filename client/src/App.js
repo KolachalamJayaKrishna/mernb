e2246a2ea10931695b1728a4021372ac6f45c657
KolachalamJayaKrishna <127821673+KolachalamJayaKrishna@users.noreply.github.com>
2024-01-04 23:05:00 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,32 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-        <div classname="entry">
-           <img src="" alt=""></img>
+        <div classname="post">
+          <div classname="image">
+           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
+          </div>
+           <div classname="content">
+           <h2>Full house battery backup comming this year</h2>
+           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           </div>       
+        </div>
+        <div classname="post">
+          <div classname="image">
+           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
+          </div>           
+          <div classname="content">
+           <h2>Full house battery backup comming this year</h2>
+           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           </div>       
+        </div>
+        <div classname="post">
+          <div classname="image">
+           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
+          </div>           
+          <div classname="content">
+           <h2>Full house battery backup comming this year</h2>
+           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           </div>       
         </div>
     </main>
   );
