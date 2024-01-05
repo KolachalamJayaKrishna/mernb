c0ef3409b303981de71fccaa6a47368188bb37af
KolachalamJayaKrishna <127821673+KolachalamJayaKrishna@users.noreply.github.com>
2024-01-05 10:23:55 +0530

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,49 @@ function App() {
   return (
     <main>
       <header>
-        <a href="" classname="logo">MyBlog</a>
+        <a href="" class="logo">MyBlog</a>
         <nav>
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
       </header>
-        <div classname="post">
-          <div classname="image">
+        <div class="post">
+          <div class="image">
            <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
           </div>
-           <div classname="content">
+           <div class="content">
            <h2>Full house battery backup comming this year</h2>
-           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           <p class="info">
+             <a class="author">Haikyuu!!</a>
+             <time>2024-01-05 10:50</time>
+           </p>
+           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
            </div>       
         </div>
-        <div classname="post">
-          <div classname="image">
+        <div class="post">
+          <div class="image">
            <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
           </div>           
-          <div classname="content">
+          <div class="content">
            <h2>Full house battery backup comming this year</h2>
-           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           <p class="info">
+             <a class="author">Haikyuu!!</a>
+             <time>2024-01-05 10:50</time>
+           </p>
+           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
            </div>       
         </div>
-        <div classname="post">
-          <div classname="image">
+        <div class="post">
+          <div class="image">
            <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
           </div>           
-          <div classname="content">
+          <div class="content">
            <h2>Full house battery backup comming this year</h2>
-           <p>Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           <p class="info">
+             <a class="author">Haikyuu!!</a>
+             <time>2024-01-05 10:50</time>
+           </p>
+           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
            </div>       
         </div>
     </main>
