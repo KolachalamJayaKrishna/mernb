6706c22530be8af30087427a8be74b539a9817a4
KolachalamJayaKrishna <127821673+KolachalamJayaKrishna@users.noreply.github.com>
2024-01-05 22:24:25 +0530

    BasicLogin

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import Post from "./Post";
 import Header from "./Header";
 import {Route,Routes} from 'react-router-dom';
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Layout />}>
       <Route index element={
-      <main>
-        <Header />
-        <Post />
-        <Post />
-        <Post /> 
-      </main>
+       <IndexPage />
       }/>
+      <Route path="/login" element={
+         <LoginPage />
+      }/>
+      </Route>
     </Routes>
   );
 }
