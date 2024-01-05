8a2ad29ad462ec5c692a36415ef717ebf41b0212
KolachalamJayaKrishna <127821673+KolachalamJayaKrishna@users.noreply.github.com>
2024-01-05 19:11:32 +0530

    extend

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,20 @@
 import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import {Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" class="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-        <div class="post">
-          <div class="image">
-           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
-          </div>
-           <div class="content">
-           <h2>Full house battery backup comming this year</h2>
-           <p class="info">
-             <a class="author">Haikyuu!!</a>
-             <time>2024-01-05 10:50</time>
-           </p>
-           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
-           </div>       
-        </div>
-        <div class="post">
-          <div class="image">
-           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
-          </div>           
-          <div class="content">
-           <h2>Full house battery backup comming this year</h2>
-           <p class="info">
-             <a class="author">Haikyuu!!</a>
-             <time>2024-01-05 10:50</time>
-           </p>
-           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
-           </div>       
-        </div>
-        <div class="post">
-          <div class="image">
-           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
-          </div>           
-          <div class="content">
-           <h2>Full house battery backup comming this year</h2>
-           <p class="info">
-             <a class="author">Haikyuu!!</a>
-             <time>2024-01-05 10:50</time>
-           </p>
-           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
-           </div>       
-        </div>
-    </main>
+    <Routes>
+      <Route index element={
+      <main>
+        <Header />
+        <Post />
+        <Post />
+        <Post /> 
+      </main>
+      }/>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div class="post">
+          <div class="image">
+           <img src="https://www.hindustantimes.com/ht-img/img/2023/05/15/550x309/Catch_the_Dream_with_a_Touch_Haikyuu_Mobile_Game_1676455379671_1684135415252.webp" alt=""></img>
+          </div>
+           <div class="content">
+           <h2>Full house battery backup comming this year</h2>
+           <p class="info">
+             <a class="author">Haikyuu!!</a>
+             <time>2024-01-05 10:50</time>
+           </p>
+           <p class="summary">Haikyu!! is a Japanese manga series written and illustrated by Haruichi Furudate. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from February 2012 to July 2020, with its chapters collected in 45 tankōbon volumes</p>
+           </div>       
+        </div>
+    );
+}
